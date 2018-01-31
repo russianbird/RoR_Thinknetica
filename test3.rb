70680d0b64c68741cb93c0f420763a926d26e76d
russianbird <iyaroslavtseva@gmail.com>
2018-02-01 00:00:00 +0300

    test6

--- test3.rb
+++ test3.rb
@@ -1,5 +1,5 @@
 print "Input your name: "
-name = gets.chomp.capitalize
+your_name = gets.chomp.capitalize
 
 print "Input your height: "
 height = gets.to_i
@@ -7,17 +7,7 @@ height = gets.to_i
 weight = height - 110
 
 if weight >= 0
-  puts "#{name}, your ideal weight is #{weight}"
+  puts "#{your_name}, your ideal weight is #{weight}"
 else
-  puts "#{name}, your ideal weight is already optimal!"
+  puts "#{your_name}, your ideal weight is already optimal!"
 end
-
-fdgjikgfhjlkfdghkdfg
-vfcvfcjvbnlkjvnjkn
-c bvcbvbxvn
-cv bdxkln dnfjzd
-jhjkj
-hvbhvjgvkgvk
-gkffjghfkhfhfjgh
-ghghjghkjgkjhgkjhg
-gfhfhjgfgdgfdhgdf
